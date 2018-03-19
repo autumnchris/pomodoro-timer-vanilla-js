5359ddebcb4a2b872cd55778a20a6e8b121096ed
Autumn <autumncbullard@gmail.com>
2018-03-19 12:03:28 -0400

    Build a function that will set a new timer session

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ workSeconds,
 breakMinutes,
 breakSeconds;
 
+function setNewTimer() {
+  workTimer = document.getElementById('work-timer-input').value * 60;
+  breakTimer = document.getElementById('break-timer-input').value * 60;
+
+  workMinutes = parseInt(workTimer / 60, 10);
+  workSeconds = parseInt(workTimer % 60, 10);
+  breakMinutes = parseInt(breakTimer / 60, 10);
+  breakSeconds = parseInt(breakTimer % 60, 10);
+
+  workMinutes = workMinutes < 10 ? '0' + workMinutes : workMinutes;
+  workSeconds = workSeconds < 10 ? '0' + workSeconds : workSeconds;
+  breakMinutes = breakMinutes < 10 ? '0' + breakMinutes : breakMinutes;
+  breakSeconds = breakSeconds < 10 ? '0' + breakSeconds : breakSeconds;
+
+  document.getElementById('workTimer').innerHTML = workMinutes + ':' + workSeconds;
+  document.getElementById('breakTimer').innerHTML = breakMinutes + ':' + breakSeconds;
+}
+
 document.getElementById('settings').addEventListener('click', function() {
   document.getElementById('modal').setAttribute('style', 'display: block');
 });
