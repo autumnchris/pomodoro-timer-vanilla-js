116256912458f58c1a1630138f6254673aa78844
Autumn <autumncbullard@gmail.com>
2018-03-19 11:41:18 -0400

    Hide modal when the Cancel button is clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 document.getElementById('settings').addEventListener('click', function() {
   document.getElementById('modal').setAttribute('style', 'display: block');
 });
+
+document.getElementById('cancel').addEventListener('click', function() {
+  document.getElementById('modal').setAttribute('style', 'display: none');
+});
