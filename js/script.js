97cffe3bc8ce0472d70a6a9cbd9feed3587d28a4
Autumn <autumncbullard@gmail.com>
2018-03-19 13:42:32 -0400

    Build function that plays timer when the play button is clicked

--- js/script.js
+++ js/script.js
@@ -26,6 +26,38 @@ function setNewTimer() {
 
 function displayTimer() {
   setNewTimer();
+
+  function countDown() {
+    timer = setInterval(playTimer, 1000);
+  }
+
+  function playTimer() {
+    if (workTimer !== 0) {
+      workTimer--;
+
+      workMinutes = parseInt(workTimer / 60, 10);
+      workSeconds = parseInt(workTimer % 60, 10);
+      workMinutes = workMinutes < 10 ? '0' + workMinutes : workMinutes;
+      workSeconds = workSeconds < 10 ? '0' + workSeconds : workSeconds;
+    }
+    else {
+      breakTimer--;
+
+      breakMinutes = parseInt(breakTimer / 60, 10);
+      breakSeconds = parseInt(breakTimer % 60, 10);
+      breakMinutes = breakMinutes < 10 ? '0' + breakMinutes : breakMinutes;
+      breakSeconds = breakSeconds < 10 ? '0' + breakSeconds : breakSeconds;
+    }
+
+    document.getElementById('workTimer').innerHTML = workMinutes + ':' + workSeconds;
+    document.getElementById('breakTimer').innerHTML = breakMinutes + ':' + breakSeconds;
+  }
+
+  document.getElementById('play-timer').addEventListener('click', function() {
+    countDown();
+    document.getElementById('play-timer').setAttribute('style', 'display: none');
+    document.getElementById('pause-timer').setAttribute('style', 'display: inline-block');
+  });
 }
 
 displayTimer();
