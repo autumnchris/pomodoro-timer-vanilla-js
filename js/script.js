5a46a37e723f7bd7099d44f3cf1dfe0813d13eb4
Autumn <autumncbullard@gmail.com>
2018-03-19 11:42:29 -0400

    Hide modal when the cursor clicks outside of the modal

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ document.getElementById('settings').addEventListener('click', function() {
 document.getElementById('cancel').addEventListener('click', function() {
   document.getElementById('modal').setAttribute('style', 'display: none');
 });
+
+document.querySelector('body').addEventListener('click', function(event) {
+
+  if (event.target.id === 'modal') {
+    document.getElementById('modal').setAttribute('style', 'display: none');
+  }
+});
