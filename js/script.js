c7f78b0e58fa06f0e6212ae146673117f9d580d6
Autumn <autumncbullard@gmail.com>
2018-03-25 19:49:45 -0400

    Update event that exits the modal when you click outside of it

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ document.getElementById('cancel').addEventListener('click', function() {
   document.getElementById('modal').setAttribute('style', 'display: none');
 });
 
-document.querySelector('body').addEventListener('click', function(event) {
+window.addEventListener('click', function(event) {
 
   if (event.target.id === 'modal') {
     document.getElementById('modal').setAttribute('style', 'display: none');
